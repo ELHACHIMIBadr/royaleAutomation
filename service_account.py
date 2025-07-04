4970d8358d248455aa750a6ab1fdc0fb45fa84ac
ELHACHIMIBadr <badr.elhachimi1998@gmail.com>
2025-07-04 01:00:13 +0100

    Update service_account.py

--- service_account.py
+++ service_account.py
@@ -10,7 +10,7 @@ LEADS_SHEET_NAME = 'Leads'
 WATCH_DB_SHEET_NAME = 'Base de données montres RH'
 
 # === Lecture depuis le fichier secret fourni par Render
-with open('/etc/secrets/royaleheurebot-6215c544a8d4.json', 'r') as f:
+with open('/etc/secrets/royaleheurebot-6215c544a8d4', 'r') as f:
     creds_dict = json.load(f)
 
 # === Création des credentials
